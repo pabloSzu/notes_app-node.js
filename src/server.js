7ea9463f10843643845ce5c373513c8cb9ab859d
pablo <pablo_szu@hotmail.com>
2022-06-04 18:23:54 -0300

    Agrego Handlebars y configuro vistas

--- src/server.js
+++ src/server.js
@@ -1,13 +1,21 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const path = require('path');
 
 //  Initializations
 const app = express();
 
-
 //  Settings
 app.set('port', process.env.PORT || 4000);
-app.set('views', path.join(__dirname + 'views'));
+app.set('views', path.join(__dirname + '/views'));
+app.engine('.hbs', exphbs.engine({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'),'layouts'),
+    partialsDir: path.join(app.get('views'),'partials'),
+    extname: '.hbs'
+}));
+    
+app.set('view engine', '.hbs');
 
 
 
@@ -19,7 +27,7 @@ app.use(express.urlencoded({extended: false}));
 
 //  Routes
 app.get('/', (req, res) => {
-    res.send('Hello world');
+    res.render('index')
 });
 
 

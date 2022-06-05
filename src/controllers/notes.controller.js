9688d18bccd7be95fbe0010b813e8b212d65e3cb
pablo <pablo_szu@hotmail.com>
2022-06-05 18:58:21 -0300

    Agrego seguridad y privcacidad

--- src/controllers/notes.controller.js
+++ src/controllers/notes.controller.js
@@ -9,19 +9,23 @@ notesCtrl.renderNoteForm = (req, res) => {
 notesCtrl.createNewNote = async (req, res) => {
     const {title, description} = req.body;
     const newNote = new Note({title, description});
+    newNote.user = req.user.id;
     await newNote.save();
     req.flash('success_msg', 'Note Added Successfully');
     res.redirect('/notes');
 }
 
 notesCtrl.renderNotes = async (req, res) => {
-    const notes = await Note.find().lean();
+    const notes = await Note.find({user: req.user.id}).lean().sort({createdAt: 'desc'});
     res.render('notes/all-notes', { notes });
 }
 
 notesCtrl.renderEditForm = async (req, res) => {
     const note = await Note.findById(req.params.id).lean();
-    console.log(Note.findById(req.params.id).lean());
+    if (note.user != req.user.id){
+        req.flash('error_msg','Not Authorized');
+        return res.redirect('/notes');
+    }
     res.render('notes/edit-note', { note });
 }
 

ff31c2a19a4fd4115e87c2906a5cf465626e6fd7
pablo <pablo_szu@hotmail.com>
2022-06-05 16:04:28 -0300

    Comunicación entre views y muestro mensajes

--- src/controllers/notes.controller.js
+++ src/controllers/notes.controller.js
@@ -10,6 +10,7 @@ notesCtrl.createNewNote = async (req, res) => {
     const {title, description} = req.body;
     const newNote = new Note({title, description});
     await newNote.save();
+    req.flash('success_msg', 'Note Added Successfully');
     res.redirect('/notes');
 }
 
@@ -28,11 +29,13 @@ notesCtrl.updateNote = async (req, res) => {
     const { title, description } = req.body;
     console.log({ title: title, description: description });
     await Note.findByIdAndUpdate(req.params.id, { title, description });
+    req.flash('success_msg', 'Note Updated Successfully');
     res.redirect("/notes");
 }
 
 notesCtrl.deleteNote = async (req, res) => {
     await Note.findByIdAndDelete(req.params.id);
+    req.flash('success_msg', 'Note Deleted Successfully');
     res.redirect('/notes');
 }
 
--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ const exphbs = require('express-handlebars');
 const path = require('path');
 const morgan = require('morgan');
 const methodOverride = require('method-override');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 //  Initializations
 const app = express();
@@ -25,9 +27,20 @@ app.set('view engine', '.hbs');
 app.use(express.urlencoded({extended: false}));
 app.use(morgan('dev'));
 app.use(methodOverride('_method'));
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUnitialized: true
+}));
+app.use(flash());
+
 
 
 //  Global Variables
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    next();
+});
 
 //  Routes
 app.use(require('./routes/index.routes'));
